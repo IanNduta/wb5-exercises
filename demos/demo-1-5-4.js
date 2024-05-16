7e7557069fc4c9725d8f49fc0049c4c213539f36
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-16 09:04:34 -0400

    finish

---
+++ demos/demo-1-5-4.js
@@ -0,0 +1,36 @@
+let menu = [
+    { id: 1, item: "Tacos", category: "Meal", price: 12.29 },
+    { id: 2, item: "Burger", category: "Meal", price: 7.29 },
+    { id: 3, item: "Salad", category: "Meal", price: 8.29 },
+    { id: 4, item: "Ice tea", category: "Drink", price: 2.19 },
+    { id: 5, item: "Coke", category: "Drink", price: 2.29 },
+
+];
+// find a single element matching a condition
+let searchId = 4;
+let matching = null;
+// let numItems = menu.length;
+
+searching = menu.find(DoesMenuObhectHaveId);
+// matching = getMenuObjectById(menu, searchId);
+
+if (matching != null) {
+    console.log(matching.item + " costs $" + matching.price);
+}
+else {
+    console.log("Item " + searchId + " not found!");
+}
+
+function DoesMenuObhectHaveId(menuObject){
+    let searchId = 4;
+    return ( menuObject.id == searchId);
+}
+
+// function getMenuObjectById(menuObjects, id){
+//     for(let menuObject of menuObjects){
+//         if(menuObject.id == id){
+//             return menuObject;
+//         }
+//     }
+//     return null;
+// }
---
+++ scripts/index.js
@@ -0,0 +1,90 @@
+"use strict";
+const toyCategoryDropDown = document.getElementById("toyCategoryDropDown");
+const toyList = document.getElementById("toyList");
+const toyDetailRow = document.getElementById("toyDetailRow");
+const ToyName = document.getElementById("toyName");
+const toyManufacturer = document.getElementById("toyManufacturer");
+const ToyAge = document.getElementById("ToyAge");
+
+window.onload = () => {
+    toyCategoryDropDown.onchange = onToCategoryDropdownChange;
+    toyList.onchange = onToyListChange;
+}
+
+function onToCategoryDropdownChange(){
+    console.log("nToCategoryDropdownChange")
+    let categoryselected = toyCategoryDropDown.value;
+
+    toyList.options.length = 0;
+
+    let toysInCategory= getToysforCategoryCode(categoryselected);
+
+
+    for(let toy of toysInCategory){
+        addToyNameToToyList(toy.name);
+    }
+
+}
+function addToyNameToToyList(toyName){
+
+        let newOption = document.createElement("option");
+        newOption.value = toyName;
+        newOption.innerHTML = toyName;
+        toyList.appendChild(newOption);
+}
+
+function getToysforCategoryCode(categoryCode){
+
+    if(categoryCode == "Action Figures"){
+        return toys.Toys["Action Figures"];
+    }
+    else if(categoryCode == "Educational Toys"){
+        return toys.Toys["Educational Toys"];
+    }
+    else if(categoryCode == "Outdoor Toys"){
+        return toys.Toys["Outdoor Toys"];
+    }
+    else{
+        console.log("error");
+    }
+    return [];
+}
+
+function getToyByName(toyName){
+    let toys = getToysforCategoryCode(categoryselected);
+    let categoryselected = toyCategoryDropDown.value;
+
+    for(let toy of toys){
+        if(toy.name == toyName){
+            return toy;
+        }
+    }
+}
+
+
+function onToyListChange(){
+    console.log("onToyListChange");
+    let selectedToyName = toyList.value;
+    // console.log(selectedToy);
+
+    let selectedToy = getToyByName(selectedToyName);
+    
+
+    ToyName.innerHTML = selectedToy.name;
+    toyManufacturer.innerHTML = selectedToy.manufacturer;
+    ToyAge.innerHTML.selectedToy.age_range;
+
+    showDetailRow();
+}
+
+function showDetailRow(){
+    toyDetailRow.style.display = "block";
+}
+
+function hideDetailRow(){
+    toyDetailRow.style.display = "none";
+    toyName.innerHTML = "";
+    toyManufacturer.innerHTML = "";
+    toyAge.innerHTML = "";
+
+}
