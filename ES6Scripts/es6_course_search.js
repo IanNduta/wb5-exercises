2eb6b0562260fedc5d3d92b8d2c638761f04c6a1
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-17 10:41:51 -0400

    finished

--- ES6Scripts/es6_course_search.js
+++ ES6Scripts/es6_course_search.js
@@ -45,8 +45,59 @@ function findCourseStartDate(arrayElement){
     }
     return foundCourseDate;
 }
+console.log("-------------------------------------------------------------------")
 
+console.log("The title of the PROJ500 course is:")
 let courseStartDate = courses.find(findCourseStartDate);
-console.log(courseStartDate.StartDate);
+console.log(`Start date of:${courseStartDate.CourseId} is ${courseStartDate.StartDate}`);
 
 //find the title for PROJ500
+// function findCourseTitle(arrayElement){
+//     let foundCourseTitle;
+//     if(arrayElement.CourseId == "PROJ500"){
+//         foundCourseTitle = arrayElement.Title;
+//     }
+//     return foundCourseTitle
+// }
+// let courseTitleFound = courses.find(findCourseTitle);
+// console.log(courseTitleFound.Title);
+
+console.log(courseStartDate.Title);
+//titles of courses thats 50 or less
+// function findCourseThats50OrLess(arrayElement){
+   
+//     if(arrayElement.Fee <= 50){
+//        return true;
+//     }
+//      return false;
+   
+// }
+
+console.log("-------------------------------------------------------------------")
+console.log("Titles of courses that cost $50 or less:")
+function findCourseThats50OrLess(arrayElement){
+    return (arrayElement.Fee <= 50);
+}
+let foundCourse50AndUnder = courses.filter(findCourseThats50OrLess);
+//loop through results to show titles...  because foundCourse50AndUnder is an array with many values
+for(let i = 0; i < foundCourse50AndUnder.length; i++ ){
+    console.log(foundCourse50AndUnder[i].Title);
+}
+
+
+console.log("-------------------------------------------------------------------")
+console.log("classes that meet in classroom1:")
+
+
+function getClassesThatMeetInRoom1(arrayElement){
+    return (arrayElement.Location == "Classroom 1")
+}
+
+let founClassesOfRoom1 = courses.filter(getClassesThatMeetInRoom1);
+//console.log(founClassesOfRoom1);
+for(let i = 0; i < founClassesOfRoom1.length; i++){
+    console.log(founClassesOfRoom1[i].CourseId);
+}
+
+
+
