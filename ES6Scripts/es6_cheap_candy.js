8917dcd6ad4366e528c722315a8319ec3e284891
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-29 22:51:10 -0400

    load

---
+++ ES6Scripts/es6_cheap_candy.js
@@ -0,0 +1,13 @@
+let products = [
+    { product: "Gummy Worms", price: 5.79 },
+    { product: "Plain M&Ms", price: 2.89 },
+    { product: "Peanut M&Ms", price: 2.89 },
+    { product: "M&Ms", price: 2.89 },
+    { product: "Swedish Fish", price: 3.79 },
+    { product: "Airheads", price: 2.19 },
+    { product: "Trolli", price: 7.99 },
+    { product: "Sour patch kids", price: 1.49 },
+    { product: "Nerds Gummy Clusters", price: 4.17 },
+    { product: "Mike and ike", price: 6.79 },
+];
+   
---
+++ NewTabs/site.js
@@ -0,0 +1,9 @@
+"use strict";
+//buttons
+const submitBtn = document.getElementById("submitBtn");
+const submitBtn2 = document.getElementById("submitBtn2");
+const submitBtn3 = document.getElementById("submitBtn3");
+
+window.onload = function(){
+    console.log("hi");
+}
