938b0a647d71ad11120d6ce7f3925212e3427395
Ian Nduta <gathonjiandutai@gmail.com>
2024-05-31 13:52:54 -0400

    deleted items

--- demo2/site.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//buttons
-const submitBtn = document.getElementById("submitBtn");
-const submitBtn2 = document.getElementById("submitBtn2");
-const submitBtn3 = document.getElementById("submitBtn3");
-
-window.onload = function(){
-    console.log("hi");
-}
